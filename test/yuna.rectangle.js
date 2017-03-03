bce7c465f7b9b5ece213ca14587d577b82820f09
12301068JiangDongyu <12301068@bjtu.edu.cn>
2017-03-03 21:11:50 +0800

    first commit

---
+++ test/yuna.rectangle.js
@@ -0,0 +1,3 @@
+var rectangle = require('../yuna.rectangle.js');
+console.log('The area of a rectangle of length 6,width 4 is '+rectangle.area(6,4));
+console.log('The circumference of a rectangle of length 6,width 4 is '+rectangle.circumference(6,4));
---
+++ yuna.rectangle.js
@@ -0,0 +1,6 @@
+exports.area = function(a,b){
+	return a*b;
+};
+exports.circumference = function(a,b){
+    return 2*(a+b);
+};
